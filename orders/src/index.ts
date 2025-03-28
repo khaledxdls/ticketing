401b7256c4fcb12d22bfc28a9bebf06390571067
Khaled Bachir Delassi <khaleddelassi03@gmail.com>
2025-03-28 14:51:31 +0100

    change

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentsCreatedListener } from "./events/listeners/payments-created-listener";
 
 const start = async () => {
-  console.log("Starting up orders service... after config");
+  console.log("Starting up orders service... after ");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
